e4edb3f3ad89fca91aa699673770897cff01e90c
Leo <leo@movio.co>
2017-03-28 14:52:59 +1300

    Added simple docker build

---
+++ docker/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -z ${1+x} ]; then echo "Usage: ./build.sh \$tag"; exit; fi
+
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o snapshotter ../.
+
+sudo docker build -t snapshotter:$1 .
